652db13970f85b84c5fb73e5397d9ad2782a2961
zhoujia6139 <zhoujia6139@126.com>
2024-03-16 17:09:53 +0800

    add api3 oracle

--- helpers/deploy-ids.ts
+++ helpers/deploy-ids.ts
@@ -45,6 +45,7 @@ export const GAS_REFUND_PROXY = "GasRefundProxy";
 export const Native_Yield_Distribute_IMPL = "NativeYieldDistributeImpl";
 export const Native_Yield_Distribute_Proxy = "NativeYieldDistributeProxy-";
 export const ERC20_Oracle_Wrapper = "ERC20OracleWrapper";
+export const API3_Oracle_Wrapper = "API3_Oracle_Wrapper";
 export const Fixed_Price_Oracle_Wrapper =
   "CLFixedPriceSynchronicityPriceAdapter";
 export const CToken_Price_Oracle_Wrapper = "ExchangeRateAssetPriceAdapter";
